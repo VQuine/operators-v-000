046ecac55048475a7de4c889f47a4651be43dfcb
Kimberly Aviles <01010110.v@gmail.com>
2018-10-31 10:57:23 +0000

    automatically backed up by learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -4,13 +4,8 @@ def unsafe?(speed)
   else
     return false
   end
-
-#  speed > 60 ? true : false
-#  speed < 40 ? true : false
 end
 
-
-
 def not_safe?(speed)
-
+  speed < 40 || speed > 60 ? return true : return false
 end
